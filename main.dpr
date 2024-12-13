b6206d2ff17b8d9a104b468389e00b44dda6e2af
k0lan4ik <kolasynnikolai@gmail.com>
2024-12-13 23:07:00 +0300

    fifty-fifty

--- main.dpr
+++ main.dpr
@@ -176,33 +176,62 @@ end;
 
 procedure FiftyFifty(var player: TPlayer;var bank:string);
 var change5: string;
+    temp_letters: string[10];
     correct_input: boolean;
-    i: integer;
+    i, f, count_letters: integer;
 begin
-  i := 1;
   correct_input := False;
   while not correct_input do
   begin
-    write('Введите буквы для обмена.');
+    i := 1;
+    f := 1;
+    count_letters := 0;
+    temp_letters := player.letters;
+    writeln('Ваш набор букв: ', player.letters);
+    write('Введите буквы для обмена: ');
     readln(change5);
     correct_input := True;
+    {if length(change5) <> 5 then
+    begin
+      correct_input := False;
+      writeln('Вы ввели неверное количество букв.');
+    end;}
+    for var l := 1 to length(change5) do
+    begin
+      if change5[l] <> ' ' then
+      count_letters := count_letters + 1;
+    end;
+    if count_letters <> 5 then
+    begin
+      correct_input := False;
+      writeln('Вы ввели неверное количество букв.');
+    end;
     while (i < 6) and (correct_input) do
     begin
-      if pos(change5[i], player.letters) = 0 then
+      if change5[f] <> ' ' then
       begin
-      correct_input := False;
-      writeln('Неверный ввод. Не все введенные вами буквы имеются в вашем наборе.');
+        if pos(change5[f], temp_letters) = 0 then
+        begin
+          correct_input := False;
+          writeln('Неверный ввод. Не все введенные вами буквы имеются в вашем наборе.');
+        end
+        else
+        begin
+          delete(temp_letters, pos(change5[f], temp_letters, 1), 1);
+        end;
+        i := i + 1;
       end;
-      i := i + 1;
+      f := f + 1;
     end;
   end;
 
   for var k := 1 to 5 do
   begin
-    delete(player.letters, pos(change5[k], player.letters, 1), 1);
+    player.letters := temp_letters;
   end;
   player.letters := player.letters + CutLetters(bank, 5);
 end;
+
 
 procedure FriendHelp(var players: TPlayers; currentPlayer: Byte);
 begin
@@ -223,7 +252,7 @@ begin
     CreateBankLetters(bank);
     ReadWordDictionary(dictionary);
     ReadPlayers(players,bank);
-    FiftyFifty(
+    FiftyFifty(players[0], bank);
     Readln;
 end.
 
